671652cb442fa6f9c1659ea20359f1681b4478f1
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-03-13 23:00:18 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const args =  process.argv;
-if (args.length <= 2) {
-    console.log("No argument");
-}
-else {
-    console.log(args[2]);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const args =  process.argv;
-if (isNaN(args[2])) {
-    console.log("Not a number");
-}
-else {
-    console.log(`My number: ${parseInt(args[2])}`)
-}
