7722949c9753899bad52baa2cf575a8698d1540e
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-03-30 21:23:54 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 URL"
-    exit 1
-fi
-
-URL=$1
-
-# Send GET request with header variable
-curl -sH "X-School-User-Id: 98" $URL
-
-echo " "
+# Takes in URL, sends GET request setting a header variable, display body
+curl -sX "GET" -H "X-School-User-Id: 98" "$1"
