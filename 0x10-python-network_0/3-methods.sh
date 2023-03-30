76c64c812a2d94077e9bad5e012034a3d8a81e27
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-03-30 21:21:39 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
 # Get HTTP methods
-if [ $# -eq 0 ]; then
-    echo "Provide a URL"
-    exit 1
-fi
-
-curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2-
+curl -sI "$1" | grep "Allow" | cut -d ' ' -f2-
