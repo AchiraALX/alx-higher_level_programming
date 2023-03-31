5eb4ad3aba873ad54db169085b1a3c6ec76a56af
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-03-31 15:26:43 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     response = requests.get(url)
     data = response.json()
     for commit in data[:10]:
-        print("{}: {}".format(commit.get('sha'),
-               commit.get('commit').get('author').get('name')))
+        print("{}: {}".format(
+            commit.get('sha'), 
+            commit.get('commit').get('author').get('name')))
