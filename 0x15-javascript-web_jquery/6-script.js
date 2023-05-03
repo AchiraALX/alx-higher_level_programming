9cff8d67dfc862b5f86e5595e0914fe63d1ca23e
AchiraALX <ed127720@students.mu.ac.ke>
2023-05-03 11:00:19 +0300

    Update 6-script.js

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,4 +1,5 @@
-// Update <header> element to New header!!! when the user clicks DIV#update_header
+// Update <header> element to New header!!!
+// when the user clicks DIV#update_header
 
 $('DIV#update_header').click(function () {
   $('header').text('New Header!!!');
