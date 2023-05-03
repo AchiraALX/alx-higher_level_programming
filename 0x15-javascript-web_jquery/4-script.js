506a3a81bb0dc8d30660fe7a86d25e713fdb97a0
AchiraALX <ed127720@students.mu.ac.ke>
2023-05-03 10:58:08 +0300

    Update 4-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,5 +1,6 @@
 // Toggle class of <header> when a user clicks DIV#toggle_header
 
 $('DIV#toggle_header').click(function () {
-  $('header').toggleClass('red green');
-});
+    $('header').toggleClass('red green');
+  }
+);
