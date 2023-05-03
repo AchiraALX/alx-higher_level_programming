9e6c834dc622652543d5765f01659dcaf14928fe
AchiraALX <ed127720@students.mu.ac.ke>
2023-05-03 11:35:34 +0300

    Create 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Update text color for <header> to #FF0000
+// use document.querySelector
+// Script will be imported from the <head>
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
